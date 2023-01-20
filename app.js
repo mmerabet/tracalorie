50b0f95d59f4c359ed0368beb6648d3db6cebbf1
mmerabet <mhmd.merabet@gmail.com>
2023-01-20 13:32:32 +0700

    controlers and data structures

---
+++ app.js
@@ -0,0 +1,45 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+  // Data structure / State
+  const data = {
+    item: [
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 0, name: "Cookie", calories: 400 },
+      { id: 0, name: "Eggs", calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+  // Public methods
+  return {
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {
+  // Public methods
+  return {};
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+  // Public methods
+  return {
+    init: function () {
+      console.log("Initializing app...");
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+App.init();
